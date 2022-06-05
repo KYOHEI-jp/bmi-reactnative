50dcae57207d1a4a1574c07346777d8b8e3b8213
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-06-05 12:02:08 +0900

    終了

--- App.tsx
+++ App.tsx
@@ -38,4 +38,80 @@ class BMIcalculator extends Component {
        this.setState({BmiResult:''})
     }
  }
- 
+ render() {
+  return (
+     <View style = {styles.container}>
+<Text style={styles.title}>BMI Calculator</Text>
+        
+        <Text  style = {styles.label}>Height</Text>
+        <TextInput style = {styles.input}
+           underlineColorAndroid = "transparent"
+           placeholder = "Height (Cm)"
+           autoCapitalize = "none"
+           onChangeText = {this.handleHeight}/>
+<Text  style = {styles.label}>Weight</Text>
+        <TextInput style = {styles.input}
+           underlineColorAndroid = "transparent"
+           placeholder = "Weight (Kg)"
+           autoCapitalize = "none"
+           onChangeText = {this.handleWeight}/>
+        
+        <TouchableOpacity
+           style = {styles.submitButton}
+           onPress = {
+              () => this.calculate(this.state.height, this.state.weight)
+           }>
+           <Text style = {styles.submitButtonText}> Calculate </Text>
+        </TouchableOpacity>
+<Text style = {styles.output}>{this.state.bmi}</Text>
+        <Text style = {styles.resultText}>{this.state.BmiResult}</Text>
+</View>
+  )
+}
+}
+export default BMIcalculator
+
+const styles = StyleSheet.create({
+container: {
+   paddingTop: 23,
+   
+},
+input: {
+   margin: 15,
+   height: 40,
+   borderWidth: 1,
+   padding: 10,
+},
+submitButton: {
+   backgroundColor: '#ff6666',
+   padding: 10,
+   margin: 15,
+   height: 40,
+},
+submitButtonText:{
+   textAlign: "center",
+   color: 'white',
+   fontSize: 18,
+},
+output:{
+   textAlign: "center",
+   fontSize: 30,
+},
+title:{
+   paddingTop:30,
+   paddingBottom:10,
+   textAlign: "center",
+   fontSize: 30,
+   fontWeight:"bold",
+},
+resultText:{
+   paddingTop:20,
+   paddingBottom:10,
+   textAlign: "center",
+   fontSize: 30,
+   color: 'blue'
+},
+label:{
+   marginLeft: 15,
+}
+})
